b39015199c6f74253d1caf687e8a023a9c2a1547
goodbro92 <taxibus@kakao.com>
2022-06-13 10:10:04 +0900

    FC

---
+++ src/test/java/com/banln/rabbitmq/BanlnRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banln.rabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanlnRabbitmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
